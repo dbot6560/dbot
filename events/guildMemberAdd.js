87f1c9fb3a57e14edc69608f7b1c629f9b6be199
Chaiwat <chaiwat5466@outlook.com>
2023-02-19 15:24:26 +0700

    remove format time

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,4 @@
 const { Events } = require('discord.js');
-const { formatTime } = require('../helpers/format');
 module.exports = {
     name: Events.GuildMemberAdd,
     once: false,
@@ -8,8 +7,7 @@ module.exports = {
         const user = member.id;
 
         if (user) {
-            const joinedAt = formatTime(member.joinedAt);
-            member.guild.channels.cache.get('1073271617779662913').send({ content: `Welcome back, ${member}! You last joined on ${joinedAt}.` });
+            member.guild.channels.cache.get('1073271617779662913').send({ content: `Welcome back, ${member}! You last joined on ${member.joinedAt}.` });
         } else {
             member.guild.channels.cache.get('1073271617779662913').send({ content: `Welcome to the server, ${member}!` });
         }
